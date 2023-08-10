ac20bb1b9cc1a0f86fac3e3864d2fdbb2612673c
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2023-08-10 14:10:49 +0200

    make paths point to user's own working area

--- Analysis/condor/paths.py
+++ Analysis/condor/paths.py
@@ -1,10 +1,12 @@
+import os
+
+#Location of the H3env environment
+H3ENV_DIR           = os.path.dirname(os.path.abspath(__file__)).replace("/H3PO/Analysis/condor", "")
 #Location of the main H3PO Analysis directory
-H3_DIR              = "/users/mrogul/Work/H3/H3PO/Analysis"
+H3_DIR              = os.path.join(H3ENV_DIR, "H3PO/Analysis")
+#Where to store condor logs for skimming jobs
+SKIM_JOB_DIR        = os.path.join(H3_DIR, "condor/skim_jobs")
+#Location of the CMSSW dir
+CMSSW_DIR           = os.path.join(H3ENV_DIR, "CMSSW_12_3_0")
 #Where to store skimmed NanoAODs
 SKIM_DIR            = "/STORE/matej/H3_skims/"
-#Where to store condor logs for skimming jobs
-SKIM_JOB_DIR        = H3_DIR+"/condor/skim_jobs"
-#Location of the CMSSW dir
-CMSSW_DIR           = "/users/mrogul/Work/H3/CMSSW_12_3_0/"
-#Location of the H3env environment
-H3ENV_DIR           = "/users/mrogul/Work/H3/"
