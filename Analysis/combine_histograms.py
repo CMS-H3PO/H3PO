105626daaab3530582f3b18ec3623f905c1ed3fa
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2023-08-16 19:25:08 +0200

    updates to go along with re-organizer condor job submission

--- Analysis/combine_histograms.py
+++ Analysis/combine_histograms.py
@@ -1,58 +1,73 @@
-import uproot
-import awkward as ak
-import matplotlib.pyplot as plt
-import hist
-from hist import Hist
-from coffea.nanoevents import NanoEventsFactory, BaseSchema
-import coffea.processor as processor
-from coffea.nanoevents import NanoEventsFactory, NanoAODSchema, schemas
-import numpy as np
-import mplhep as hep
-from Selection import *
+import sys
 from os import listdir, getcwd, system, chdir
 from os.path import isfile, join
+from optparse import OptionParser
+
 
 def remove_root_files(list_of_root_files):
     for file in list_of_root_files:
         system('rm ' + file)
 
-def combine_histograms(dataset):
+def combine_histograms(identifier, rmFiles=False, startsWithRegion=True, mvFiles=False, fit_dir="fit", startsWithId=False):
+    
     regions = ["semiBoosted_pass","semiBoosted_fail","Boosted_pass","Boosted_fail"]
+    
     for region in regions:
         list_of_root_files = []
-        try:
-            system('rm {0}_{1}.root'.format(dataset,region))
-        except:
-            print('{0}_{1}.root didn\'t exist before'.format(dataset,region))
-            
-        for file in listdir(getcwd()):
-            if not isfile(join(getcwd(), file)):
+        cwd = getcwd()
+        
+        for file in listdir(cwd):
+            if not isfile(join(cwd, file)):
                 continue
-            if dataset in file and '.root' in file and '.png' not in file and region in file:
+            if (file.startswith(identifier) if startsWithId else ('_'+identifier+'-') in file) and file.endswith('.root') and (file.startswith(region) if startsWithRegion else ('_'+region+'_') in file):
                 list_of_root_files.append(file)
 
-        system("hadd -f {0}_{1}_50.root {2}".format(dataset,region, " ".join(list_of_root_files)))
-        remove_root_files(list_of_root_files)
-        system("mv {0}_{1}_50.root ./rootfiles/".format(dataset,region))
+        system("hadd -f {0}_{1}_50.root {2}".format(identifier,region, " ".join(list_of_root_files)))
+        if rmFiles:
+            remove_root_files(list_of_root_files)
+        if mvFiles:
+            system("mkdir -p {0}".format(fit_dir))
+            system("mv {0}_{1}_50.root {2}".format(identifier,region,fit_dir))
+
 
 if __name__ == '__main__':
+    # usage description
+    usage = "Usage: python %prog [options] \nExample: python %prog -i condor_jobs_<timestamp>"
+    
+    # input parameters
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-i", "--input", dest="input",
+                      help="Input directory name (This parameter is mandatory)",
+                      metavar="INPUT")
+    
+    parser.add_option("-f", "--fit_dir", dest="fit_dir",
+                      help="Fit directory name (This parameter is optional, default: fit)",
+                      default="fit",
+                      metavar="FIT_DIR")
+    
+    parser.add_option("--delete", dest="delete", action='store_true',
+                      help="Delete Condor output root files",
+                      default=False)
+
+    (options, args) = parser.parse_args()
+
+    # make sure all necessary input parameters are provided
+    if not options.input:
+        print('Mandatory parameters missing')
+        print('')
+        parser.print_help()
+        sys.exit(1)
+
+    chdir(options.input)
+
     datasets = ["QCD500", "QCD700", "QCD2000", "QCD1000", "QCD1500", "TTbarHadronic", "TTbarSemileptonic",
                 "JetHT2017B", "JetHT2017C", "JetHT2017D", "JetHT2017E", "JetHT2017F"]
+    
     for dataset in datasets:
-        combine_histograms(dataset)
+        combine_histograms(dataset, options.delete)
 
-
-    chdir('/users/bchitrod/HHH/H3PO/Analysis/rootfiles/')
-    regions = ["semiBoosted_pass","semiBoosted_fail","Boosted_pass","Boosted_fail"]
     processes = ["QCD","TTbar","JetHT2017"]
 
     for process in processes:
-        for region in regions:
-            list_of_root_files = []
-            for file in listdir(getcwd()):
-                if not isfile(join(getcwd(), file)):
-                    continue
-                if process in file and '.root' in file and '.png' not in file and region in file and '_50' in file:
-                    list_of_root_files.append(file)
-            system("hadd {0}_{1}_50.root {2}".format(process,region," ".join(list_of_root_files)))
-            system("mv {0}_{1}_50.root ./fit/".format(process,region))
+        combine_histograms(process, False, False, True, options.fit_dir, True)
