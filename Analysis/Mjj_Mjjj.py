13db92465e0e45e0deb902b7ed072d6a1911e113
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2023-09-07 23:07:47 +0200

    dropping scaling by unity

--- Analysis/Mjj_Mjjj.py
+++ Analysis/Mjj_Mjjj.py
@@ -6,7 +6,7 @@ from hist import Hist
 from Selection import *
 from Util import *
 
-def plotboosted(boosted_SR_fail, boosted_SR_pass,boosted_VR_fail,boosted_VR_pass,scale,process):
+def plotboosted(boosted_SR_fail, boosted_SR_pass,boosted_VR_fail,boosted_VR_pass,process):
 
     trijet_mass_SR_fail = (boosted_SR_fail[:,0]+boosted_SR_fail[:,1]+boosted_SR_fail[:,2]).mass
     trijet_mass_SR_pass = (boosted_SR_pass[:,0]+boosted_SR_pass[:,1]+boosted_SR_pass[:,2]).mass
@@ -17,22 +17,18 @@ def plotboosted(boosted_SR_fail, boosted_SR_pass,boosted_VR_fail,boosted_VR_pass
     j3_SR_fail_bin = hist.axis.Regular(label="Boosted Signal Fail Trijet Mass [GeV]", name="trijet_mass_SR_fail", bins=120, start=0, stop=6000)
     j3_SR_fail_hist = Hist(j3_SR_fail_bin)
     j3_SR_fail_hist.fill(trijet_mass_SR_fail=trijet_mass_SR_fail)
-    j3_SR_fail_hist *= scale
 
     j3_SR_pass_bin = hist.axis.Regular(label="Boosted Signal Pass Trijet Mass [GeV]", name="trijet_mass_SR_pass", bins=120, start=0, stop=6000)
     j3_SR_pass_hist = Hist(j3_SR_pass_bin)
     j3_SR_pass_hist.fill(trijet_mass_SR_pass=trijet_mass_SR_pass)
-    j3_SR_pass_hist *= scale
 
     j3_VR_fail_bin = hist.axis.Regular(label="Boosted Validation Fail Trijet Mass [GeV]", name="trijet_mass_VR_fail", bins=120, start=0, stop=6000)
     j3_VR_fail_hist = Hist(j3_VR_fail_bin)
     j3_VR_fail_hist.fill(trijet_mass_VR_fail=trijet_mass_VR_fail)
-    j3_VR_fail_hist *= scale
 
     j3_VR_pass_bin = hist.axis.Regular(label="Boosted Validation Pass Trijet Mass [GeV]", name="trijet_mass_VR_pass", bins=120, start=0, stop=6000)
     j3_VR_pass_hist = Hist(j3_VR_pass_bin)
     j3_VR_pass_hist.fill(trijet_mass_VR_pass=trijet_mass_VR_pass)
-    j3_VR_pass_hist *= scale
 
     dijet1_mass_SR_fail = (boosted_SR_fail[:,0]+boosted_SR_fail[:,1]).mass
     dijet2_mass_SR_fail = (boosted_SR_fail[:,0]+boosted_SR_fail[:,2]).mass
@@ -53,33 +49,29 @@ def plotboosted(boosted_SR_fail, boosted_SR_pass,boosted_VR_fail,boosted_VR_pass
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet1_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet2_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet3_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
-    mjj_vs_mjjj_SR_fail *= scale
 
     j2_SR_pass_bin = hist.axis.Regular(label="Boosted Signal Pass Dijet Mass [GeV]", name="dijet_mass_SR_pass", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_SR_pass = Hist(j3_SR_pass_bin, j2_SR_pass_bin)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet1_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet2_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet3_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
-    mjj_vs_mjjj_SR_pass *= scale
 
     j2_VR_fail_bin = hist.axis.Regular(label="Boosted Validation Fail Dijet Mass [GeV]", name="dijet_mass_VR_fail", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_VR_fail = Hist(j3_VR_fail_bin, j2_VR_fail_bin)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet1_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet2_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet3_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
-    mjj_vs_mjjj_VR_fail *= scale
 
     j2_VR_pass_bin = hist.axis.Regular(label="Boosted Validation Pass Dijet Mass [GeV]", name="dijet_mass_VR_pass", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_VR_pass = Hist(j3_VR_pass_bin, j2_VR_pass_bin)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet1_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet2_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet3_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
-    mjj_vs_mjjj_VR_pass *= scale
 
     return j3_SR_fail_hist,j3_SR_pass_hist,j3_VR_fail_hist,j3_VR_pass_hist,mjj_vs_mjjj_SR_fail,mjj_vs_mjjj_SR_pass,mjj_vs_mjjj_VR_fail,mjj_vs_mjjj_VR_pass
 
 
-def plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semiboosted_SR_fail_jet, semiboosted_SR_pass_jet,semiboosted_VR_fail_fatjet,semiboosted_VR_pass_fatjet,semiboosted_VR_fail_jet,semiboosted_VR_pass_jet,scale,process):
+def plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semiboosted_SR_fail_jet, semiboosted_SR_pass_jet,semiboosted_VR_fail_fatjet,semiboosted_VR_pass_fatjet,semiboosted_VR_fail_jet,semiboosted_VR_pass_jet,process):
 
     trijet_mass_SR_fail = (semiboosted_SR_fail_fatjet[:,0]+semiboosted_SR_fail_fatjet[:,1]+semiboosted_SR_fail_jet[:,0]['i0']+semiboosted_SR_fail_jet[:,0]['i1']).mass
     trijet_mass_SR_pass = (semiboosted_SR_pass_fatjet[:,0]+semiboosted_SR_pass_fatjet[:,1]+semiboosted_SR_pass_jet[:,0]['i0']+semiboosted_SR_pass_jet[:,0]['i1']).mass
@@ -90,22 +82,18 @@ def plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semib
     j3_SR_fail_bin = hist.axis.Regular(label="semiBoosted Signal Fail Trijet Mass [GeV]", name="trijet_mass_SR_fail", bins=120, start=0, stop=6000)
     j3_SR_fail_hist = Hist(j3_SR_fail_bin)
     j3_SR_fail_hist.fill(trijet_mass_SR_fail=trijet_mass_SR_fail)
-    j3_SR_fail_hist *= scale
 
     j3_SR_pass_bin = hist.axis.Regular(label="semiBoosted Signal Pass Trijet Mass [GeV]", name="trijet_mass_SR_pass", bins=120, start=0, stop=6000)
     j3_SR_pass_hist = Hist(j3_SR_pass_bin)
     j3_SR_pass_hist.fill(trijet_mass_SR_pass=trijet_mass_SR_pass)
-    j3_SR_pass_hist *= scale
 
     j3_VR_fail_bin = hist.axis.Regular(label="semiBoosted Validation Fail Trijet Mass [GeV]", name="trijet_mass_VR_fail", bins=120, start=0, stop=6000)
     j3_VR_fail_hist = Hist(j3_VR_fail_bin)
     j3_VR_fail_hist.fill(trijet_mass_VR_fail=trijet_mass_VR_fail)
-    j3_VR_fail_hist *= scale
 
     j3_VR_pass_bin = hist.axis.Regular(label="semiBoosted Validation Pass Trijet Mass [GeV]", name="trijet_mass_VR_pass", bins=120, start=0, stop=6000)
     j3_VR_pass_hist = Hist(j3_VR_pass_bin)
     j3_VR_pass_hist.fill(trijet_mass_VR_pass=trijet_mass_VR_pass)
-    j3_VR_pass_hist *= scale
 
     dijet1_mass_SR_fail = (semiboosted_SR_fail_fatjet[:,0]+semiboosted_SR_fail_fatjet[:,1]).mass
     dijet2_mass_SR_fail = (semiboosted_SR_fail_fatjet[:,0]+semiboosted_SR_fail_jet[:,0]['i0']+semiboosted_SR_fail_jet[:,0]['i1']).mass
@@ -126,28 +114,24 @@ def plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semib
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet1_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet2_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
     mjj_vs_mjjj_SR_fail.fill(dijet_mass_SR_fail=dijet3_mass_SR_fail,trijet_mass_SR_fail=trijet_mass_SR_fail)
-    mjj_vs_mjjj_SR_fail *= scale
 
     j2_SR_pass_bin = hist.axis.Regular(label="Semiboosted Signal Pass Dijet Mass [GeV]", name="dijet_mass_SR_pass", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_SR_pass = Hist(j3_SR_pass_bin, j2_SR_pass_bin)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet1_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet2_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
     mjj_vs_mjjj_SR_pass.fill(dijet_mass_SR_pass=dijet3_mass_SR_pass,trijet_mass_SR_pass=trijet_mass_SR_pass)
-    mjj_vs_mjjj_SR_pass *= scale
 
     j2_VR_fail_bin = hist.axis.Regular(label="Semiboosted Validation Fail Dijet Mass [GeV]", name="dijet_mass_VR_fail", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_VR_fail = Hist(j3_VR_fail_bin, j2_VR_fail_bin)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet1_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet2_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
     mjj_vs_mjjj_VR_fail.fill(dijet_mass_VR_fail=dijet3_mass_VR_fail,trijet_mass_VR_fail=trijet_mass_VR_fail)
-    mjj_vs_mjjj_VR_fail *= scale
 
     j2_VR_pass_bin = hist.axis.Regular(label="Semiboosted Validation Pass Dijet Mass [GeV]", name="dijet_mass_VR_pass", bins=80, start=0, stop=4000)
     mjj_vs_mjjj_VR_pass = Hist(j3_VR_pass_bin, j2_VR_pass_bin)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet1_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet2_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
     mjj_vs_mjjj_VR_pass.fill(dijet_mass_VR_pass=dijet3_mass_VR_pass,trijet_mass_VR_pass=trijet_mass_VR_pass)
-    mjj_vs_mjjj_VR_pass *= scale
 
     return j3_SR_fail_hist,j3_SR_pass_hist,j3_VR_fail_hist,j3_VR_pass_hist,mjj_vs_mjjj_SR_fail,mjj_vs_mjjj_SR_pass,mjj_vs_mjjj_VR_fail,mjj_vs_mjjj_VR_pass
 
@@ -168,7 +152,6 @@ if __name__ == "__main__":
     print(process)
     year = "2017"
    
-    scale = 1
     numberOfGenEvents = 0.
     if ("JetHT" not in process):
         numberOfGenEvents = getNumberOfGenEvents(input)
@@ -205,8 +188,8 @@ if __name__ == "__main__":
             cutFlowHistos[r].SetBinContent(i+1, event_counts[r][key])
             cutFlowHistos[r].GetXaxis().SetBinLabel(i+1, key)
     
-    j3_SR_fail_boosted,j3_SR_pass_boosted,j3_VR_fail_boosted,j3_VR_pass_boosted,mjj_vs_mjjj_SR_fail_boosted,mjj_vs_mjjj_SR_pass_boosted,mjj_vs_mjjj_VR_fail_boosted,mjj_vs_mjjj_VR_pass_boosted = plotboosted(boosted_SR_fail,boosted_SR_pass,boosted_VR_fail,boosted_VR_pass,scale,process)                  
-    j3_SR_fail_semiboosted,j3_SR_pass_semiboosted,j3_VR_fail_semiboosted,j3_VR_pass_semiboosted,mjj_vs_mjjj_SR_fail_semiboosted,mjj_vs_mjjj_SR_pass_semiboosted,mjj_vs_mjjj_VR_fail_semiboosted,mjj_vs_mjjj_VR_pass_semiboosted = plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semiboosted_SR_fail_jet, semiboosted_SR_pass_jet,semiboosted_VR_fail_fatjet,semiboosted_VR_pass_fatjet,semiboosted_VR_fail_jet,semiboosted_VR_pass_jet,scale,process)
+    j3_SR_fail_boosted,j3_SR_pass_boosted,j3_VR_fail_boosted,j3_VR_pass_boosted,mjj_vs_mjjj_SR_fail_boosted,mjj_vs_mjjj_SR_pass_boosted,mjj_vs_mjjj_VR_fail_boosted,mjj_vs_mjjj_VR_pass_boosted = plotboosted(boosted_SR_fail,boosted_SR_pass,boosted_VR_fail,boosted_VR_pass,process)                  
+    j3_SR_fail_semiboosted,j3_SR_pass_semiboosted,j3_VR_fail_semiboosted,j3_VR_pass_semiboosted,mjj_vs_mjjj_SR_fail_semiboosted,mjj_vs_mjjj_SR_pass_semiboosted,mjj_vs_mjjj_VR_fail_semiboosted,mjj_vs_mjjj_VR_pass_semiboosted = plotsemiboosted(semiboosted_SR_fail_fatjet, semiboosted_SR_pass_fatjet,semiboosted_SR_fail_jet, semiboosted_SR_pass_jet,semiboosted_VR_fail_fatjet,semiboosted_VR_pass_fatjet,semiboosted_VR_fail_jet,semiboosted_VR_pass_jet,process)
     with uproot.recreate(os.path.join(output, "Histograms_{0}-{1}".format(process, ofile))) as fout:
         fout[f"j3_SR_pass_boosted"] = j3_SR_pass_boosted
         fout[f"j3_VR_pass_boosted"] = j3_VR_pass_boosted
