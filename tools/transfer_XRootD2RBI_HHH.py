94d516a7ae406e0dfe70916749c7ef69aa371409
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2023-12-04 19:24:14 +0100

    generalizing a bit the script

--- tools/transfer_XRootD2RBI_HHH.py
+++ tools/transfer_XRootD2RBI_HHH.py
@@ -1,15 +1,19 @@
 import os
+import sys
 from datetime import datetime
 
 # this script works with Python 3
 # execute 'voms-proxy-init -rfc -voms cms -valid 168:00' before running the script
 
-r_txt = open('/users/ferencek/HHH/H3PO/Analysis/HHH_samples_2017.txt', 'r')
+# open a file containing a list of remote files
+r_txt = open(sys.argv[1], 'r')
 remote_files = r_txt.read().splitlines()
 r_txt.close()
 
 #for r in remote_files:
     #print (r)
+
+year = sys.argv[2]
 
 t_txt = open('transferred_files.txt', 'a+')
 t_txt.seek(0)
@@ -32,13 +36,13 @@ for r in remote_files:
     mx = split[2]
     my = split[3]
     
-    dest = f'/STORE/matej/H3_skims/2017/XToYHTo6B_{mx}_{my}'
+    dest = f'/STORE/matej/H3_skims/{year}/XToYHTo6B_{mx}_{my}'
     
     cmd = f"mkdir -p {dest}"
     print('\n' + cmd)
     os.system(cmd)
 
-    cmd = f'xrdcp -f "root://eoscms.cern.ch/{r}" "{dest}"'
+    cmd = f'xrdcp -f "root://xrootd-cms.infn.it//{r}" "{dest}"'
     print(cmd + '\n')
     exit_code = os.system(cmd)
     if exit_code == 0:
