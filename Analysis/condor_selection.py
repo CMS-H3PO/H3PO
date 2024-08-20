62fd951db37f95e00a29c9bd57969bb2c88c00ee
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2024-08-21 00:14:22 +0200

    explicitly specifying the amount of requested memory for Condor jobs

--- Analysis/condor_selection.py
+++ Analysis/condor_selection.py
@@ -102,6 +102,7 @@ if __name__ == '__main__':
                 job_file.write('universe    = vanilla\n')
                 job_file.write('initialdir  = ' + initial_dir + '\n')
                 job_file.write('getenv = False\n')
+                job_file.write('RequestMemory = 2000\n')
                 
                 job_file.write('log    = ' + join(condor_dir_logs, 'log-' + dataset_job + '.log') + '\n')
                 job_file.write('output = ' + join(condor_dir_logs, 'tmp-' + dataset_job + '.out') + '\n')
